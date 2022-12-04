038debb75cdf87d2d55244af86edf3dfc2ce95a2
yeqingliu103 <119683442+yeqingliu103@users.noreply.github.com>
2022-12-04 17:19:21 +0800

    pull commit

--- src/main/java/com/jun/git/GitTest.java
+++ src/main/java/com/jun/git/GitTest.java
@@ -16,6 +16,7 @@ public class GitTest {
         System.out.println("hot-fix！");
         System.out.println("push test！别名—http");
         System.out.println("push test！ssh");
+        System.out.println("pull test！");
     }
 }
 

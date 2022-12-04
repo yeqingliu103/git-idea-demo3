08002bf30eaa573ad55f7af019f492fb78b40ab9
jun <1034553602@qq.com>
2022-12-04 15:04:29 +0800

    second commit

--- src/main/java/com/jun/git/GitTest.java
+++ src/main/java/com/jun/git/GitTest.java
@@ -9,6 +9,7 @@ package com.jun.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git！");
+        System.out.println("hello git2！");
     }
 }
 

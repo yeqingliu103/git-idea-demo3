7768ef4ca9b8c6395c8d8e99e430373126d486ea
jun <1034553602@qq.com>
2022-12-04 14:56:11 +0800

    first commit

---
+++ src/main/java/com/jun/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.jun.git;
+
+/**
+ * @ClassName GitTest
+ * @Author Jun
+ * @Date 2022/12/4 14:46
+ * @Description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git！");
+    }
+}
+
